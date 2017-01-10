7e09bd6e0a003457e54bc2358359e5ee82a66ef8
Nikhil Rajpal <nikrdc@gmail.com>
2017-01-09 23:23:09 -0800

    use uber 1.2 api

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import os
 import config
-import requests
 import json
 import random
 from yelpapi import YelpAPI
-from nominatim import NominatimReverse
+from geopy.geocoders import Nominatim
 from flask import Flask, render_template 
 from flask.ext.wtf import Form
 from wtforms import IntegerField, DecimalField, SubmitField
 from wtforms.validators import Required, NumberRange
 from flask.ext.script import Manager, Shell, Server
 from time import time
+from uber_rides.session import Session
+from uber_rides.client import UberRidesClient
 
 
 
 # Config
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = config.SECRET_KEY
 
 manager = Manager(app)
@@ -30,16 +31,21 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 def page_not_found(e):
     return render_template('404.html'), 404
 
-yelp_api = YelpAPI(config.YELP_CONSUMER_KEY, config.YELP_CONSUMER_SECRET, config.YELP_TOKEN, 
-                   config.YELP_TOKEN_SECRET)
+yelp_api = YelpAPI(config.YELP_CONSUMER_KEY, config.YELP_CONSUMER_SECRET, 
+                   config.YELP_TOKEN, config.YELP_TOKEN_SECRET)
 
-nomrev = NominatimReverse()
+uber_session = Session(server_token=config.UBER_SERVER_TOKEN)
+uber_client = UberRidesClient(uber_session)
+
+geolocator = Nominatim()
 
 categories = 'amusementparks,aquariums,beaches,bowling,escapegames,gokarts,' +\
              'hiking,lakes,parks,skatingrinks,skydiving,zoos,arcades,' +\
              'gardens,movietheaters,festivals,jazzandblues,museums,' +\
              'musicvenues,observatories,opera,planetarium,psychic_astrology,' +\
              'bars,comedyclubs,danceclubs,karaoke,poolhalls,fleamarkets,food'
+
+product_url = 'https://api.uber.com/v1/products'
 
 
 
@@ -52,7 +58,8 @@ class RequestForm(Form):
             Required(message="Choose a number from 1 to 50"), 
             NumberRange(min=1, max=50, message="Choose a number from 1 to 50"),
         ], 
-        default=7,)
+        default=7,
+    )
     latitude = DecimalField('Latitude', validators=[Required()])
     longitude = DecimalField('Longitude', validators=[Required()])
     submit = SubmitField('Go')
@@ -67,31 +74,41 @@ def index():
     if form.validate_on_submit():
         amount = form.amount.data
         start_lat = form.latitude.data
-        start_lon = form.longitude.data
-        product_url = 'https://api.uber.com/v1/products'
-        product_parameters = {
-            'server_token': config.UBER_SERVER_TOKEN,
-            'latitude': start_lat,
-            'longitude': start_lon,
-        }
-        product_response = requests.get(product_url, params=product_parameters)
-        product_data = product_response.json()
-        price_details = product_data['products'][0]['price_details']
-        service_fees = 0
-        for fee in price_details['service_fees']:
-            service_fees += fee['fee']
-        travel_money = amount - price_details['base'] - service_fees
-        radius_miles = travel_money / (price_details['cost_per_distance'] + 
-                       price_details['cost_per_minute'] * 3)
-        radius_meters = radius_miles * 1609
-        latlong = str(start_lat)+', '+str(start_lon)
-        town = str(nomrev.query(lat=start_lat, lon=start_lon)['address']['city'])
-        yelp_data = yelp_api.search_query(location=town, 
-                                          sort=2,
-                                          category_filter=categories,
-                                          radius_filter=radius_meters,
-                                          cll=latlong,
-                                          limit=20,)
+        start_long = form.longitude.data
+        print("initialized form data")        
+
+        uber_products_response = uber_client.get_products(start_lat, start_long)
+        uber_products = uber_products_response.json.get('products')
+        for i in xrange(len(uber_products)):
+            if uber_products[i]['display_name'] == 'uberX':
+                uber_index = i
+                break
+        uber_product_id = uber_products[uber_index]['product_id'] 
+        uber_product_response = uber_client.get_product(uber_product_id)
+        uber_price_details = uber_product_response.json['price_details']
+        print("retrieved uber product and price details")
+
+        uber_service_fees = 0
+        for fee in uber_price_details['service_fees']:
+            uber_service_fees += fee['fee']
+        travel_money = amount - uber_price_details['base'] - uber_service_fees
+        print("determined remaining travel cash")
+
+        radius_meters = travel_money / (uber_price_details['cost_per_distance']
+                        + uber_price_details['cost_per_minute'] * 3) * 1609
+        start_coords = str(start_lat) + ', ' + str(start_long)
+        town = str(geolocator.reverse(start_coords).raw['address']['city'])
+        print("found " + town)
+        yelp_data = yelp_api.search_query(
+            location=town, 
+            sort=2,
+            category_filter=categories,
+            radius_filter=radius_meters,
+            cll=start_coords,
+            limit=10,
+        )
+        print("grabbed yelp results")
+
         destinations = yelp_data['businesses']
         i = 0
         while (i < len(destinations)) and (destinations[i]['rating'] > 3.5):
@@ -106,38 +123,38 @@ def index():
                 chosen = random.choice(destinations)
             else:
                 return render_template('womp.html')
-            # coordinates sometime not available
-            price_url = 'https://api.uber.com/v1/estimates/price'
             chosen_lat = chosen['location']['coordinate']['latitude']
-            chosen_lon = chosen['location']['coordinate']['longitude']
-            price_parameters = {
-                'server_token': config.UBER_SERVER_TOKEN,
-                'start_latitude': start_lat,
-                'start_longitude': start_lon,
-                'end_latitude': chosen_lat,
-                'end_longitude': chosen_lon,
-            }
-            price_response = requests.get(price_url, params=price_parameters)
-            price_data = price_response.json()
-            product_id = price_data['prices'][0]['product_id']
-            high_estimate = price_data['prices'][0]['high_estimate']
+            chosen_long = chosen['location']['coordinate']['longitude']
+            uber_price_estimate = uber_client.get_price_estimates(
+                start_latitude=start_lat,
+                start_longitude=start_long,
+                end_latitude=chosen_lat,
+                end_longitude=chosen_long,
+                seat_count=1
+            ).json.get('prices')
+            uberX_price = uber_price_estimate[uber_index]
+            product_id = uberX_price['product_id']
+            high_estimate = uberX_price['high_estimate']
+        print("picked " + chosen['name'])
+        
         chosen_name = chosen['name'].replace(' ', '%20')
-        d_address = ' '.join(chosen['location']['display_address'])
+        display_address = ' '.join(chosen['location']['display_address'])
         chosen_address = '%20'.join(chosen['location']['display_address']).replace(' ','%20')
         deep_link = "uber://?client_id=" + config.UBER_CLIENT_ID +\
                     "&action=setPickup&pickup=my_location&dropoff[latitude]=" +\
-                    str(chosen_lat) + "&dropoff[longitude]=" + str(chosen_lon) +\
+                    str(chosen_lat) + "&dropoff[longitude]=" + str(chosen_long) +\
                     "&dropoff[nickname]=" + chosen_name +\
                     "&dropoff[formatted_address]=" + chosen_address +\
-                    "&product_id="+product_id
+                    "&product_id=" + product_id
+        
         return render_template('boom.html', 
                                deep_link=deep_link, 
                                d_name=chosen['name'], 
-                               d_address=d_address,
-                               d_cost=price_data['prices'][0]['estimate'],
+                               d_address=display_address,
+                               d_cost=uberX_price['estimate'],
                                d_rating=chosen['rating'],
                                d_url=chosen['mobile_url'],
-                               d_product=price_data['prices'][0]['display_name'],)
+                               d_product=uberX_price['display_name'],)
     else:
         return render_template('index.html', form=form)
         
